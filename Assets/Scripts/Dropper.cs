e73dca72e6bf3e5e61e4c601999553f7730da1dd
sleepydollx <amanda182rezky@gmail.com>
2026-08-02 18:18:09 +0700

    adding a new feature to the Dropper script that makes the object invisible and disables gravity until a certain time has passed. The MeshRenderer is now disabled and the Rigidbody's useGravity property is set to false in the Start method.

--- Assets/Scripts/Dropper.cs
+++ Assets/Scripts/Dropper.cs
@@ -4,15 +4,23 @@ public class Dropper : MonoBehaviour
 {
     [SerializeField] float timeToWait = 3f;
 
+    MeshRenderer MyMeshRenderer;
+    Rigidbody MyRigidbody;
+
     void Start()
     {
-        
+      MyMeshRenderer = GetComponent<MeshRenderer>();
+      MyRigidbody = GetComponent<Rigidbody>();
+
+      MyMeshRenderer.enabled = false;
+      MyRigidbody.useGravity = false; 
+
     }
 
     
     void Update()
     {
-        if (Time.time > 3)
+        if (Time.time > timeToWait)
         {
             Debug.Log("Look up! :)");
         }
