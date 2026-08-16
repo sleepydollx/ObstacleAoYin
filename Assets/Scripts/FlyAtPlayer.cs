3030a10e9cc19a3c000f26672d00a8df2cab3132
sleepydollx <amanda182rezky@gmail.com>
2026-08-16 21:03:21 +0700

    add new speed variable to control the movement speed of the object towards the player.

--- Assets/Scripts/FlyAtPlayer.cs
+++ Assets/Scripts/FlyAtPlayer.cs
@@ -2,16 +2,18 @@ using UnityEngine;
 
 public class FlyAtPlayer : MonoBehaviour
 {
+    [SerializeField] float speed = 1.0f;
     [SerializeField] Transform playerTransform;
     Vector3 playerPosition;
     
     void Start()
     {
-        
+        playerPosition = playerTransform.position;
     }
 
     void Update()
     {
-        transform.position = Vector3.MoveTowards(transform.position, playerTransform.position, 1f);
+        transform.position = 
+        Vector3.MoveTowards(transform.position, playerPosition, Time.deltaTime*speed);
     }
 }
